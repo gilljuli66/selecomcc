4508d2174edc81f94c7ffb5070744126a6c2cf45
gilljuli66 <93138338+gilljuli66@users.noreply.github.com>
2022-04-01 11:04:33 +0200

    commit 002 : Up apps Nursery pour demo

---
+++ nursery/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Plant Nursery',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Plants and customers management',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+        'security/ir.model.access.csv',
+        'views/shop_menu.xml',
+        ]
+}
